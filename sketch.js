815203211360795e3f1b6f1b63e84b75961089f4
Tai Karir <taikarir@Tais-MacBook-Air.local>
2023-03-14 11:15:24 -0400

    initial commit, create canvas

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let canvasSize = 800;
+
+function setup() {
+    createCanvas(canvasSize, canvasSize);
+    textSize(32);
+    textAlign(CENTER,CENTER);
+    strokeWeight(3);
+}
+
+function draw() {
+    background(0, 0, 0);
+}
